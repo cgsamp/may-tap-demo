71fa63e8157f3b93d4f4a0d0456761df7d78704d
Christopher Samp <chrisgsamp@gmail.com>
2022-05-06 19:01:50 -0400

    .

--- update-techdocs.sh
+++ update-techdocs.sh
@@ -1,5 +1,4 @@
-export AWS_ACCESS_KEY_ID=AKIATVCKPRD4LQBZ62HC
-export AWS_SECRET_ACCESS_KEY=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
+source ./secrets.sh
 export AWS_REGION=us-east-1
 
 npx @techdocs/cli generate --source-dir ./components/yelb-ui --output-dir ./site
